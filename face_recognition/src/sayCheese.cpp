d2428cb556cdc0061bf80d352ef0f473aa745a8f
knil-sama <cols.knil@gmail.com>
2015-01-15 10:02:53 +0100

    added file

--- face_recognition/src/sayCheese.cpp
+++ face_recognition/src/sayCheese.cpp
@@ -1,12 +1,26 @@
-#include<iostream>
+#include <iostream>
 #include <stdio.h>
 #include "../header/image_processing.h"
+
+using namespace std;
+
 int main(int argc, const char* argv[]){
 	if(argc != 3){
 		return -1;
 		cout << "Error" << endl;
 	}
-	cout <<
-	cout << endl;
+	//mock histo
+	cout << "{" ;
+	for(int count = 0; count <255; count++){
+		cout << '"';
+		cout << count;
+		cout << '"';
+		cout << " : ";
+		cout <<  count;
+		if(count < 254){
+			cout << ",";
+		}  
+	}
+	cout << "}" << endl;
 	return 0;
 }
